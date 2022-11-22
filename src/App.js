11c09afd642cba3c8368c9cd5ff294d4cfa00bc7
Sofya Vibe <sofyavibe@gmail.com>
2022-11-22 09:07:53 +0300

    added functionality to counter app using useState hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const Increment = () => {
+    setCount(count + 1);
+  };
+  const Decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={Decrement}>- Минус</button>
+        <button className="plus" onClick={Increment}>Плюс +</button>
       </div>
     </div>
   );
