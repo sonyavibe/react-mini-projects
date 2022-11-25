380e72ecb70786b121aa1af3817a874ce8771645
Sofya Vibe <sofyavibe@gmail.com>
2022-11-25 08:47:01 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
   return (
     <div className="App">
