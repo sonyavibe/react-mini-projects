6aaaeed13a646b33ea31dfc3097cd57773f53a74
Sofya Vibe <sofyavibe@gmail.com>
2022-11-28 08:28:29 +0300

    added skeleton for a currency converter app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
 import React from 'react';
+import { Block } from './Block';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState('');
-  const [users, setUsers] = React.useState([]);
-  const [invites, setInvites] = React.useState([]);
-  const [success, setSuccess] = React.useState(false);
-  React.useEffect(() => {
-    fetch('https://reqres.in/api/users')
-      .then(res => res.json())
-      .then(json => {
-        setUsers(json.data);
-      }).catch(err => {
-        console.warn(err);
-        alert('Error with userlist fetching!');
-      });
-  }, []);
-    const onChangeSearchValue = (event) => {
-      setSearchValue(event.target.value);
-    };
-    const onClickInvite = (id) => {
-      if(invites.includes(id)) {
-        setInvites(prev => prev.filter(_id => _id !== id));
-      } else {
-        setInvites(prev => [...prev, id]);
-      }
-    };
-    const onClickSendInvites = () => {
-      setSuccess(true);
-    };
   return (
     <div className="App">
-      {
-        success ? <Success count={invites.length}/> : 
-        <Users onChangeSearchValue={onChangeSearchValue}
-             searchValue={searchValue}
-             invites={invites}
-             onClickInvite={onClickInvite}
-             onClickSendInvites={onClickSendInvites}
-             items={users}/>
-             
-      }
-      
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
